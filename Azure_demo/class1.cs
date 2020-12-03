6d00e877b7ff6f2296d63213f0e2a97a3dd60cb6
anuragpatidar17 <anurag.patidar@yahoo.in>
2020-12-03 19:16:16 +0530

    first

---
+++ Azure_demo/class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NPOI.SS.UserModel;
+using System.Threading;
+using NPOI.Util;
+using NPOI.XSSF.UserModel;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+
+
+namespace Azure_demo
+{
+    
+    public class class1
+    {
+        [Test]
+        public void SearchForWord()
+        {
+            var driver = new ChromeDriver();
+
+            {
+                FileStream file = new FileStream(@"/Users/anuragpatidar/Projects/Azure_demo/Book1.xlsx", FileMode.Open, FileAccess.Read);
+                XSSFWorkbook workbook = new XSSFWorkbook(file);
+                ISheet sheet = workbook.GetSheet("Sheet1");
+
+                var value = string.Format(sheet.GetRow(0).GetCell(0).StringCellValue);
+
+                //Notice navigation is slightly different than the Java version
+                //This is because 'get' is a keyword in C#
+                driver.Navigate().GoToUrl("http://www.google.com/");
+
+                // Find the text input element by its name
+                IWebElement query = driver.FindElement(By.Name("q"));
+
+                Thread.Sleep(5000);
+
+                query.SendKeys(value);
+
+                Thread.Sleep(5000);
+
+                string check_value = query.GetAttribute("value");
+
+
+
+
+
+                Console.WriteLine(check_value);
+
+                driver.Quit();
+
+
+            }
+        }
+    }
+}
