2a37a470d3964b57d5812901f579d94a020292e5
anuragpatidar17 <anurag.patidar@yahoo.in>
2020-12-04 15:12:44 +0530

    five

---
+++ Azure_demo/class2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using NUnit.Framework;
+using System.IO;
+using System.Reflection;
+
+
+namespace Azure_demo
+{
+
+    public class class2
+    {
+        [Test]
+        public void SearchForWord2()
+        {
+            var driver = new ChromeDriver();
+
+            {
+                
+                driver.Navigate().GoToUrl("https://www.facebook.com/");
+
+                // Find the text input element by its name
+                IWebElement query = driver.FindElement(By.Name("q"));
+
+                Thread.Sleep(5000);
+
+                query.SendKeys("hola hola");
+
+                Thread.Sleep(5000);
+
+                driver.Quit();
+
+
+            }
+        }
+    }
+}
